c93a6c5e8fb55132222d511ecc6b35e2e16ecbe5
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-18 09:52:46 +0100

    uitwerking

--- calculator.php
+++ calculator.php
@@ -3,6 +3,7 @@ echo("Welke operatie wil je uitvoeren? (+, -)");
 $oparator = readline();
 
 if ($oparator == "+") {
+    
     echo("getal nummer 1?");
     $getal1 = readline();
     echo("en getal nummer 2?");
@@ -10,6 +11,7 @@ if ($oparator == "+") {
     echo (int) ($getal1+$getal2);
 }
 elseif ($oparator = "-") {
+
     echo ("getal nummer 1?");
     $getal3 = readline();
     echo("en getal nummer 2?");
